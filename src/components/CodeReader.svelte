53c66940c919a6fdf10a276a9a296e8c926a5294
Jose Arturo <arturotoll@gmail.com>
2022-11-15 22:55:23 -0500

    otro prro

--- src/components/CodeReader.svelte
+++ src/components/CodeReader.svelte
@@ -1,33 +1,37 @@
 <script>
-    import Quagga from "quagga";
     import { onMount } from "svelte";
     export let result;
-    onMount(() => {
-        const width = window.innerWidth - 32;
-        Quagga.init(
-            {
-                inputStream: {
-                    name: "Live",
-                    type: "LiveStream",
-                    target: document.querySelector("#camera"), // Or '#yourElement' (optional)
-                    constraints: {
-                        width,
-                        height: width,
-                    },
-                },
-                decoder: {
-                    readers: ["ean_reader", "ean_8_reader"],
+
+    onMount(async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: {
+                facingMode: {
+                    ideal: "environment",
                 },
             },
-            function (err) {
-                if (err) return;
-                Quagga.start();
-            }
-        );
-        Quagga.onDetected(function (data) {
-            result = data.codeResult.code;
+            audio: false,
         });
+        const videoEl = document.querySelector("#stream");
+        videoEl.srcObject = stream;
+        await videoEl.play();
+
+        const barcodeDetector = new BarcodeDetector({ formats: ["ean_13"] });
+        window.setInterval(async () => {
+            const barcodes = await barcodeDetector.detect(videoEl);
+            if (barcodes.length <= 0) return;
+
+            result = barcodes.map((barcode) => barcode.rawValue).join("");
+            //alert(barcodes.map((barcode) => barcode.rawValue));
+        }, 1000);
     });
 </script>
 
-<div id="camera" />
+<video id="stream" />
+<p>El codigo es: {result}</p>
+
+<style>
+    video {
+        width: 300px;
+        aspect-ratio: 1;
+    }
+</style>
