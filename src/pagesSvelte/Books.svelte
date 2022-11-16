0343836bb54a93a1295d88c76843a94fbae1721b
Jose Arturo <arturotoll@gmail.com>
2022-11-16 10:05:33 -0500

    add more details

--- src/pagesSvelte/Books.svelte
+++ src/pagesSvelte/Books.svelte
@@ -1,10 +1,12 @@
 <script>
     import Button from "../components/Button.svelte";
+
     import CodeReader from "../components/CodeReader.svelte";
     import Input from "../components/Input.svelte";
     import Loading from "../components/Loading.svelte";
     import Modal from "../components/Modal.svelte";
     import Book from "../Models/Book";
+    import Category from "../Models/Category";
     import Editorial from "../Models/Editorial";
 
     let camera = false;
@@ -12,10 +14,20 @@
     let dialog;
     let loading = false;
     let editorials = [];
-    let book;
+    let categories = [];
+    let book = {
+        count: 1,
+        editorial: { name: "Sin Editorial" },
+        category: { name: "Sin Area" },
+        author: { name: "Anonimo" },
+        position: 1,
+    };
     const saveBook = async (isbn) => {
         Editorial.find({ limit: 100 }).then((data) => {
             editorials = data.editorials.results;
+        });
+        Category.find({ limit: 100 }).then((data) => {
+            categories = data.categorys.results;
         });
         loading = true;
         camera = false;
@@ -26,21 +38,13 @@
         const data = await response.json();
         const object = data.items && data.items[0];
         if (object) {
-            book = {
-                name: object.volumeInfo.title,
-                author: {
-                    name: object.volumeInfo.authors
-                        ? object.volumeInfo.authors[0]
-                        : "Anonimo",
-                },
-                isbn: code,
-                count: 1,
-                image: object.volumeInfo.imageLinks?.thumbnail,
-                category: { name: "Sin Area" },
-                editorial: {
-                    name: object.volumeInfo.publisher || "Sin Editorial",
-                },
-            };
+            book.name = object.volumeInfo.title;
+            if (object.volumeInfo.authors)
+                book.author.name = object.volumeInfo.authors[0];
+            book.isbn = code;
+            book.image = object.volumeInfo.imageLinks?.thumbnail;
+            if (object.volumeInfo.publisher)
+                book.editorial.name = object.volumeInfo.publisher;
         }
 
         loading = false;
@@ -62,7 +66,7 @@
     <Modal bind:dialog>
         {#if loading}
             <Loading />
-        {:else if book}
+        {:else if book.name}
             <section class="details">
                 <h2>Informacion del libro</h2>
                 <picture>
@@ -81,8 +85,25 @@
                         list="editorials"
                         bind:value={book.editorial.name}
                     />
+                    <span>Area</span>
+                    <input
+                        type="text"
+                        name="book[category]"
+                        id="book[category]"
+                        list="categories"
+                        bind:value={book.category.name}
+                    />
+                    <span>Cantidad</span>
+                    <input type="number" bind:value={book.count} />
+                    <span>Ubicacion</span>
+                    <input type="number" bind:value={book.position} />
                     <datalist id="editorials">
                         {#each editorials as { name, _id }}
+                            <option value={name} />
+                        {/each}
+                    </datalist>
+                    <datalist id="categories">
+                        {#each categories as { name }}
                             <option value={name} />
                         {/each}
                     </datalist>
@@ -92,7 +113,10 @@
                         loading = true;
                         Book.saveByIsbn(book).then(() => {
                             $dialog.close();
-                            book = null;
+                            book.name = null;
+                            book.author = { name: "Anonimo" };
+                            book.count = 1;
+                            book.editorial.name = "Sin Editorial";
                         });
                     }}>Confimar</Button
                 >
